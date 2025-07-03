b93e7a7a41c445eb8ea740aa3acb2e15dd393281
MrCalvooo <terminatoresplandor@gmail.com>
2025-07-03 11:26:59 +0200

    operaciones

--- Basic/Calculadora/src/Main.kt
+++ Basic/Calculadora/src/Main.kt
@@ -1,14 +1,44 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+    var num1: Int
+    var num2: Int
+    var operacion: String
+    do {
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+        println("Que operacion desea realizar? (suma, resta, multiplicacion, division): ")
+        operacion = readln().lowercase()
+
+        println("Ingrese el primer numero: ")
+        num1 = readln().toInt()
+
+        println("Ingrese el segundo numero: ")
+        num2 = readln().toInt()
+
+        if (num2 == 0) {
+            println("Numero no valido")
+        }
+
+    } while (num2 == 0)
+
+    val resultado = realizarOperacion(operacion, num1, num2)
+
+    println("$operacion de $num1 y $num2 es: $resultado")
 }
+
+fun realizarOperacion(operacion: String, num1: Int, num2: Int): Int {
+    var resultado = 0
+    // Se utiliza when para determinar la operación a realizar
+    when (operacion) {
+        "suma" -> resultado = num1 + num2
+        "resta" -> resultado = num1 - num2
+        "multiplicacion" -> resultado = num1 * num2
+        "division" -> resultado = num1 / num2
+        // Si se ingresa una operación no válida, se realiza una suma por defecto
+        else -> {
+            resultado = num1 + num2
+        }
+    }
+    return resultado
+}
+
